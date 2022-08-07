f2c98900207aabde258106dc8d79008e1f87ebda
rodrigo debian <correo.rodrigo@gmail.com>
2022-08-07 12:28:52 -0500

    resize path

--- resizeImg/resizeFiles.sh
+++ resizeImg/resizeFiles.sh
@@ -6,37 +6,42 @@ IMAGES_PATH="./"
 
 procesar_imagenes () {
     # si param 1 no es directorio, salir
-    if [ ! -d "$1" ]; then
-	showHelp()
+    if [ ! -d "$1" ]; then	
+	echo 'Folder not found:' $1 >&2
 	exit 1
     fi
 
     # si param 2 no es numero salir
-    if [![ "$2" =~ ^[0-9]+$ ]]; then
-	showHelp()
-	exit 1
+    if ! [[ $2 =~ ^[0-9]?([.][0-9]+)?$ ]]; then
+	echo 'Not a number:' $2 >&2
+	exit 1 
     fi
 
-    IMAGES_PATH=${$1/}
+    IMAGES_PATH=$(dirname "$1")"/"$(basename "$1")
+    echo $IMAGES_PATH
 
-    python3 $RESIZE_CMD_PATH $IMAGES_PATH $2
-    python3 $TO_WEBP_CMD_PATH $IMAGES_PATH
-    rm $IMAGES_PATH/*.jpg
+    #python3 $RESIZE_CMD_PATH $IMAGES_PATH $2
+    #python3 $TO_WEBP_CMD_PATH $IMAGES_PATH
+    #rm $IMAGES_PATH/*.jpg
 
-    for filename in $IMAGES_PATH/*_resized.webp; do 
-	[ -f "$filename" ] || continue
-	mv "$filename" "${filename/_resized.webp/.webp}"
-    done
+    #for filename in $IMAGES_PATH/*_resized.webp; do 
+#	[ -f "$filename" ] || continue
+#	mv "$filename" "${filename/_resized.webp/.webp}"
+#    done
 }
 
 showHelp()
 {
   echo "Cambia el tamaño de jpg y los convierte a .webp" >&2
-  echo "Uso: ./resizeFiles.sh FOLDER REDUCCION" >&2
+  echo "Uso: ./resizeFiles.sh [Dir-Path] [float-size]" >&2
+  echo ""
   echo "Ejemplo reducir a la mitad"
-  echo ":$ ./resizeFiles.sh ~/imagenes .5" >&2
+  echo "$ ./resizeFiles.sh ~/imagenes 0.5" >&2
 }
 
+if [ $# -ne 2 ] ; then
+  showHelp
+  exit 1
+fi
 
-https://www.gnu.org/savannah-checkouts/gnu/bash/manual/bash.html#Shell-Parameter-Expansion
-https://stackoverflow.com/questions/9018723/what-is-the-simplest-way-to-remove-a-trailing-slash-from-each-parameter
+procesar_imagenes $1 $2
