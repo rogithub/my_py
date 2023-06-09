7b0538089357e1bae365c8d10c9f45824df0a7ea
ro <rosec@protonmail.com>
2023-06-09 09:57:48 -0500

    resize to 1024 mx

--- resizeImg/resizeImg.py
+++ resizeImg/resizeImg.py
@@ -2,18 +2,18 @@
 from PIL import Image
 import os, sys
 
-size = 1024, 1024
+size = 1040, 780
 
 def resize(path):
     dirs = os.listdir( path )
     for item in dirs:
         if os.path.isfile(path+item):
             with Image.open(path+item) as im:                                
-                imResize = im.thumbnail(size) 
+                imResize = im.thumbnail(size, PIL.Image.ANTIALIAS) 
                 imResize.save(f + '_resized.jpg', 'JPEG')
 
 if __name__ == "__main__":
     path = sys.argv[1];
-    print('Resizing to 1024 x 1024 max path={}'.format(path))
+    print('Resizing to 1040 x 780 max path={}'.format(path))
     resize(path)
     print ('Done.')
